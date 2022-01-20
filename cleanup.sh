9bad052e790e2e4c823a9e9194c423edda23da6a
Antoine Deleporte <adeleporte@vmware.com>
2022-01-20 19:02:53 +0100

    fix permissions

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cmd=$(echo "\"service idps-reporting-service stop && /opt/vmware/bin/corfu_tool_runner.py  -t ids_event_data -n security_data_service --port 9040 -o clearTable && service idps-reporting-service start && curl -X PUT -H \"Content-Type: application/json\" \"localhost:9200/security_data_service_metadata/_doc/security_data_service?pretty\" -d' {\"clusterId\" : \"-1\"}'\"")
-echo $cmd
+sshpass -p $NSX_ROOT_PASSWORD ssh -o StrictHostKeyChecking=no root@$TF_VAR_nsx_manager service idps-reporting-service stop
 
-echo $cmd | xargs sshpass -p $TF_VAR_nsx_password ssh -o StrictHostKeyChecking=no $TF_VAR_nsx_username@$TF_VAR_nsx_manager
+sshpass -p $NSX_ROOT_PASSWORD ssh -o StrictHostKeyChecking=no root@$TF_VAR_nsx_manager /opt/vmware/bin/corfu_tool_runner.py  -t ids_event_data -n security_data_service --port 9040 -o clearTable
+
+sshpass -p $NSX_ROOT_PASSWORD ssh -o StrictHostKeyChecking=no root@$TF_VAR_nsx_manager service idps-reporting-service start
+
+sshpass -p $NSX_ROOT_PASSWORD ssh -o StrictHostKeyChecking=no root@$TF_VAR_nsx_manager curl -X PUT -H "Content-Type:application/json" "localhost:9200/security_data_service_metadata/_doc/security_data_service?pretty" -d '{\"clusterId\":\"-1\"}'
